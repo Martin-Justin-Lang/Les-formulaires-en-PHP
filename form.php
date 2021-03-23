9f729ce9c39837f155e31551c3e783ced4d1bc93
Martin-Justin-Lang <martin.lang67@gmail.com>
2021-03-23 10:04:00 +0100

    1er commit

---
+++ form.php
@@ -0,0 +1,16 @@
+<pre>
+    <?php
+    var_dump($_GET);
+    echo "<br/>" ;
+    echo  $_GET['user_name']; 
+    echo "<br/>" ;
+    echo  $_GET['user_email'];
+    echo "<br/>" ;
+    echo  $_GET['user_telephone'];
+    echo "<br/>" ;
+    echo  $_GET['user_sujet'];
+    echo "<br/>" ;
+    echo  $_GET['user_message'];
+    echo "<br/>" ;
+?>
+</pre>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Formulaire en PHP</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form  action="http://localhost:8000/" method="get">
+        <div>
+          <label  for="nom">Nom :</label>
+          <input  type="text"  id="nom"  name="user_name">
+        </div>
+
+        <div>
+          <label  for="courriel">Courriel :</label>
+            <input  type="email"  id="courriel"  name="user_email">
+        </div>
+
+        <div>
+          <label  for="telephone">Telephone :</label>
+          <input  type="telephone"  id="telephone"name="user_telephone"></input>
+        </div>
+        
+        <div>
+        <label  for="Sujet">Sujet :</label>
+        <select id="sujet" name="user_sujet">
+            <option value="">--Choisi ton plat--</option>
+            <option value="Pizza">Pizza</option>
+            <option value="Kebab">Kebab</option>
+            <option value="Tacos">Tacos</option>
+            <option value="Burger">Burger</option>
+            <option value="Coucous">Couscous</option>
+            <option value="Paëla">Paëla</option>
+         </select>
+       </div>
+
+
+        <div>
+          <label  for="message">Message :</label>
+          <textarea  id="message"  name="user_message"></textarea>
+        </div>
+
+        <div  class="button">
+          <button  type="submit">Envoyer votre message</button>
+        </div>
+      </form>
+  </body>
+  <?php include'form.php'; ?>
+</html>
